d712e9a6fa3ad331f55488dbc1a6ee6de506f696
j-shifat-18 <info.jahirulsifat@gmail.com>
2025-07-23 02:05:31 +0600

    add footer

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import useAuth from '../../Hooks/useAuth';
 import { Link, useNavigate } from 'react-router';
+import SkyTowerLogo from '../SkyTowerLogo/SkyTowerLogo';
 
 const Navbar = () => {
   const { user, logOutUser } = useAuth();
@@ -30,9 +31,7 @@ const Navbar = () => {
   return (
     <nav className="w-full  shadow flex items-center justify-between px-6 h-16 relative z-20">
       {/* Logo and Name */}
-      <div className="flex items-center gap-2 select-none">
-        <span className="font-bold text-2xl tracking-wide">Sky<span className='text-primary'>Tower</span></span>
-      </div>
+        <SkyTowerLogo></SkyTowerLogo>
       {/* Nav Links */}
       <div className="flex items-center gap-8">
         <Link to="/" className="hover:text-secondary transition-colors font-medium text-base">Home</Link>
---
+++ src/Components/SkyTowerLogo/SkyTowerLogo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SkyTowerLogo = () => {
+    return (
+        <div className="flex items-center gap-2 select-none">
+        <span className="font-bold text-2xl tracking-wide">Sky<span className='text-primary'>Tower</span></span>
+      </div>
+    );
+};
+
+export default SkyTowerLogo;
